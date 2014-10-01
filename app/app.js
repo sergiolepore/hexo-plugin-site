5f2301760c6b7cc3257dd4069e9e90b2473a8b3b
Sergio Lepore <sergio.d.lepore@gmail.com>
2014-10-01 00:25:07 -0300

    Update ember-cli app to 0.0.46

--- app/app.js
+++ app/app.js
@@ -1,14 +1,16 @@
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  modulePrefix: 'hexo-plugin-site', // TODO: loaded via config
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
 
-loadInitializers(App, 'hexo-plugin-site');
+loadInitializers(App, config.modulePrefix);
 
 export default App;

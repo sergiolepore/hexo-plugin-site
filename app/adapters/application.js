551a418e72d12953932a6bff651190c27e57e16f
Sergio Lepore <sergio.d.lepore@gmail.com>
2014-10-05 20:06:11 -0300

    Change the way the configuration is accessed

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
+import ENV from 'hexo-plugin-site/config/environment';
 
 export default DS.RESTAdapter.extend({
-  host: window.HexoPluginSite.APP.apiHost,
-  namespace: '/v1'
+  host: ENV.APP.apiHost,
+  namespace: '/v1',
+  coalesceFindRequests: true
 });

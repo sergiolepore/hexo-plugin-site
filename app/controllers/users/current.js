9ebb60d5a5e1b5e480754ff05f574bc09f81ceb3
Sergio Lepore <sergio.d.lepore@gmail.com>
2014-10-25 21:57:59 -0300

    Add change password actions

--- app/controllers/users/current.js
+++ app/controllers/users/current.js
@@ -1,9 +1,11 @@
 import Ember   from 'ember';
 import swalert from 'sweetAlert';
+import ENV     from 'hexo-plugin-site/config/environment';
 
 var UsersCurrentController = Ember.ObjectController.extend({
 
-  isEditing : false,
+  isEditing          : false,
+  isChangingPassword : false,
 
   actions : {
 
@@ -33,9 +35,47 @@ var UsersCurrentController = Ember.ObjectController.extend({
       });
     },
 
-    updatePassword : function() {
-      // TODO: implement updatePassword logic.
-      // PUT api/users/password
+    changePassword : function() {
+      this.set('isChangingPassword', true);
+    },
+
+    cancelPassword : function() {
+      this.setProperties({
+        isChangingPassword : false,
+        oldPassword        : null,
+        newPassword        : null
+      });
+    },
+
+    savePassword : function() {
+      var passwordData = this.getProperties('oldPassword', 'newPassword');
+      var apiUrl       = '%baseurl%/users/password'.replace('%baseurl%', ENV.APP.apiBaseEndpoint);
+      var _this        = this;
+
+      Ember.$.ajax({
+        url  : apiUrl,
+        data : passwordData,
+        type : 'PUT'
+      }).then(function() {
+        // everything ok
+        swalert('Awesome!', 'Your password has been successfully updated.', 'success');
+
+        _this.setProperties({
+          isChangingPassword : false,
+          oldPassword        : null,
+          newPassword        : null,
+        });
+      }, function(reason) {
+        console.log(reason);
+
+        if (reason.status === 401 || reason.status === 403) {
+          swalert('Oops...', 'It seems that your data is invalid.', 'error');
+        } else if (reason.status === 400) {
+          swalert('Oops...', 'Your .', 'error');
+        } else {
+          swalert('Oops :(', 'This is embarrassing, but there was an unknown error while trying to save the data. Try again later.', 'error');
+        }
+      });
     },
 
     // permanently deletes the user account
@@ -47,7 +87,8 @@ var UsersCurrentController = Ember.ObjectController.extend({
       this.get('model').rollback();
 
       this.setProperties({
-        isEditing : false,
+        isEditing          : false,
+        isChangingPassword : false,
       });
     }
 

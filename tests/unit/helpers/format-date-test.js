99b33653ee241aae036605ff3490b80af0c55159
Sergio Lepore <sergio.d.lepore@gmail.com>
2014-11-19 23:08:51 -0300

    Fix format-date test

--- tests/unit/helpers/format-date-test.js
+++ tests/unit/helpers/format-date-test.js
@@ -5,9 +5,9 @@ import {
 module('FormatDateHelper');
 
 test('it applies a format to a given date', function() {
-  var sourceDate = '2014-11-20T01:49:06.757Z';
+  var sourceDate = '2014-11-20T01:49:06';
   var format     = 'dddd, MMMM Do YYYY, h:mm:ss a';
-  var targetDate = 'Wednesday, November 19th 2014, 10:49:06 pm';
+  var targetDate = 'Thursday, November 20th 2014, 1:49:06 am';
   var result     = formatDate(sourceDate, format);
 
   ok(result === targetDate);
